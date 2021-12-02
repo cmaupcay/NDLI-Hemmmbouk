62ea9269466d671f01aa9252387b7ad65e242e70
Arpharnator <49691625+Arpharnator@users.noreply.github.com>
2021-12-02 23:55:09 +0100

    Create Bateau.php

---
+++ www/api/modele/Bateau.php
@@ -0,0 +1,19 @@
+<?php
+include_once 'modele/ModeleBD.php';
+
+    include_once 'modele/Facture.php';
+
+    class Bateau extends ModeleBD
+    {
+        public function informations(): array
+        { return ['idBateau', 'nomBateau']; }
+		public function table() : string { return 'bateau'; }
+
+        public function __construct(?int $id = null, ?BD &$bd = null)
+        { parent::__construct($id, $bd); }
+		
+        
+        private $nomBateau;
+        public function nomBateau() : ?string { return $this->nomBateau; }
+        public function modifier_nomBateau(?string $valeur) { $this->nomBateau = $valeur; }
+?>
