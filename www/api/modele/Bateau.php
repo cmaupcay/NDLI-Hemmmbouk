7a176857eb696c4a35c2cb365473eb8cd69479db
Arpharnator <49691625+Arpharnator@users.noreply.github.com>
2021-12-03 01:53:22 +0100

    Update Bateau.php

--- www/api/modele/Bateau.php
+++ www/api/modele/Bateau.php
@@ -1,14 +1,18 @@
 <?php
-include_once 'modele/ModeleBD.php';
+include_once __RACINE__ . 'modele/ModeleBD.php';
+include_once __RACINE__ . 'modele/Article.php';
 
     class Bateau extends ModeleBD
     {
+	public $article;
         public function informations(): array
         { return ['idBateau', 'nomBateau']; }
 		public function table() : string { return 'bateau'; }
 
         public function __construct(?int $id = null, ?BD &$bd = null)
-        { parent::__construct($id, $bd); }
+        { parent::__construct($id, $bd);
+	  $this->article = new Article();
+	}
 		
         
         private $nomBateau;
